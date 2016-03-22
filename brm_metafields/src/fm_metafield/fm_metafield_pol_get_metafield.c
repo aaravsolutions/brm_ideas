bb04147fdf843be0fec816f81a5bb5d4064be5a1
Prabha1991 <prabha.muthusamy@aaravsolution.comSecret@123>
2016-03-22 11:37:31 +0530

    new GET opcode

--- brm_metafields/src/fm_metafield/fm_metafield_pol_get_metafield.c
+++ brm_metafields/src/fm_metafield/fm_metafield_pol_get_metafield.c
@@ -37,12 +37,11 @@ static  char    Sccs_id[] = "@(#)%Portal Version: fm_metafield_pol_get_metafield
 #include <malloc.h>
 #include <string.h>
 #include "pcm.h"
-#include "ops/newpri1.h"
 #include "cm_fm.h"
 #include "pin_errs.h"
 #include "pin_cust.h"
 #include "pinlog.h"
-
+#include "ops/newpri1.h"
 
 
 #define FILE_SOURCE_ID  "fm_metafield_pol_get_metafield.c (1.7)"
@@ -147,69 +146,134 @@ fm_get_meta_field(
         pin_flist_t             **out_flistpp,
         pin_errbuf_t            *ebufp)
 {
-        pin_flist_t     *items_flistp = NULL;
+	pin_flist_t     *keys_flistp = NULL;
+        pin_flist_t     *search_flistp = NULL;
         pin_flist_t     *flistp = NULL; 
         poid_t          *s_pdp = NULL; 
         poid_t          *bi_pdp = NULL; 
-        int32           status = 0;
         int64           database = 0;
         int32           sflags = 256;
         int32           result = PIN_BOOLEAN_FALSE;
         void            *vp = NULL;
         pin_decimal_t   *zero_decimal =  pin_decimal("0.0", ebufp);
 	pin_flist_t	*r_flistp = NULL;
-	int 		   count =0;
+	char                    *name = NULL;
+	char                    *class_name = NULL;
+	char                    *default_value;
+	int32           count = 0;
+	int32           cnt_flds = 0;
+	int32                   elemid = 1;
+	pin_errbuf_t            any_field_ebuf;
+        pin_cookie_t            cookie = NULL;
+        pin_fld_num_t           field = 0;
+        int32                   rec_id = 0;
+	void            *type = NULL;
+
         if (PIN_ERR_IS_ERR(ebufp)) {
                 return;
         }
         PIN_ERR_CLEAR_ERR(ebufp);
-
-
-
-	//Count FIELDS array 
-	count = PIN_FLIST_ELEM_COUNT(in_flistp,PIN_FLD_VALUES,ebufp); 
-			   
-	if (count==0)
-		return;
+	PIN_ERR_CLEAR_ERR(&any_field_ebuf);
 	
+	PIN_ERR_LOG_FLIST(PIN_ERR_LEVEL_DEBUG, "fm_get_meta_field input flistp", in_flistp);
+	bi_pdp = (poid_t *) PIN_FLIST_FLD_GET(in_flistp,
+                                PIN_FLD_POID,0, ebufp);
 
-	else if (count >0)
-	{
+	count = PIN_FLIST_ELEM_COUNT (in_flistp,
+                        PIN_FLD_KEYS, ebufp);
+	if (count == 0)
+        {
 
- /*********************************************************
+                 pin_set_err(ebufp, PIN_ERRLOC_FM,
+                        PIN_ERRCLASS_SYSTEM_DETERMINATE,
+                        PIN_ERR_BAD_OPCODE, 0, 0, count);
+                PIN_ERR_LOG_EBUF(PIN_ERR_LEVEL_ERROR,
+                        "op_get_meta_field search flistp", ebufp);
+
+                *out_flistpp = NULL;
+                return;
+        }
+
+	else {
+	keys_flistp = PIN_FLIST_ELEM_GET (in_flistp, PIN_FLD_KEYS,
+                PIN_ELEMID_ANY, 0, ebufp);
+	while (PIN_FLIST_ANY_GET_NEXT(keys_flistp, &field,
+                                &rec_id, &cookie, &any_field_ebuf) != NULL ){
+        cnt_flds = cnt_flds + 1;
+	if (field == PIN_FLD_NAME){
+ 	name = (char *)PIN_FLIST_FLD_GET(keys_flistp, PIN_FLD_NAME, 1, ebufp);
+	}
+	if (field == PIN_FLD_CLASS_NAME){
+       class_name = PIN_FLIST_FLD_GET(keys_flistp, PIN_FLD_CLASS_NAME, 0, ebufp );
+	}
+	if (field == PIN_FLD_TYPE){
+	type = PIN_FLIST_FLD_GET(keys_flistp, PIN_FLD_TYPE, 0, ebufp );			
+	}
+	}
+	if ( cnt_flds == 1)
+        {
+        vp = (void *)"select X from /metafield where F1 = V1 ";
+        }
+        if ( cnt_flds == 2)
+        {
+        vp = (void *)"select X from /metafield where F1 = V1 and F2 = V2 ";
+        }
+        if ( cnt_flds == 3)
+        {
+        vp = (void *)"select X from /metafield where F1 = V1 and F2 = V2 and F3 = V3 ";
+        }
+
+	 /*********************************************************
          * Create the search flist to search in the item objects
          *********************************************************/
-	bi_pdp = PIN_FLIST_FLD_GET(in_flistp, PIN_FLD_POID, 0, ebufp );
-        items_flistp = PIN_FLIST_CREATE(ebufp);
+        search_flistp = PIN_FLIST_CREATE(ebufp);
         database = PIN_POID_GET_DB(bi_pdp);
         s_pdp = PIN_POID_CREATE(database, "/search", -1, ebufp);
-        PIN_FLIST_FLD_PUT(items_flistp, PIN_FLD_POID, (void *)s_pdp, ebufp);
-        PIN_FLIST_FLD_SET(items_flistp, PIN_FLD_TEMPLATE,
-         (void *) "select X  from /metafield where  F1 = V1 ", ebufp);
-        PIN_FLIST_FLD_SET(items_flistp, PIN_FLD_FLAGS, (void *)&sflags, ebufp);
+        PIN_FLIST_FLD_PUT(search_flistp, PIN_FLD_POID, (void *)s_pdp, ebufp);
+       /******************************************************************
+         * Set the search template.
+         ******************************************************************/
+        PIN_FLIST_FLD_SET(search_flistp, PIN_FLD_TEMPLATE, vp, ebufp);
+        PIN_FLIST_FLD_SET(search_flistp, PIN_FLD_FLAGS, (void *)&sflags, ebufp);
+
+	/*********************************************************
+         * Add the search arguments.
+         *********************************************************/
+	if (name != NULL) {
+        flistp = PIN_FLIST_ELEM_ADD(search_flistp, PIN_FLD_ARGS, elemid++, ebufp);
+        PIN_FLIST_FLD_SET(flistp, PIN_FLD_NAME, name, ebufp);
+	}
 
         /*********************************************************
          * Add the search arguments.
          *********************************************************/
+	if (class_name != NULL) {
+        flistp = PIN_FLIST_ELEM_ADD(search_flistp, PIN_FLD_ARGS, elemid++, ebufp);
+        PIN_FLIST_FLD_SET(flistp, PIN_FLD_CLASS_NAME, class_name, ebufp);
+        }
+	
+	/*********************************************************
+         * Add the search arguments.
+         *********************************************************/
+        if (type != NULL) {
+        flistp = PIN_FLIST_ELEM_ADD(search_flistp, PIN_FLD_ARGS, elemid++, ebufp);
+        PIN_FLIST_FLD_SET(flistp, PIN_FLD_TYPE, type, ebufp);
+        }
 
-        flistp = PIN_FLIST_ELEM_ADD(items_flistp, PIN_FLD_ARGS, 1, ebufp);
-        
 
-	PIN_FLIST_FLD_SET(flistp, PIN_FLD_CLASS_NAME, (void *)"ASSOGSM", ebufp);
-
-      
         /*********************************************************
          * Add the result array for the search.
          *********************************************************/
 
-        flistp = PIN_FLIST_ELEM_ADD(items_flistp, PIN_FLD_RESULTS, 0, ebufp);
-        PIN_FLIST_FLD_SET(flistp, PIN_FLD_TYPE,(void *)NULL, ebufp);
+        flistp = PIN_FLIST_ELEM_ADD(search_flistp, PIN_FLD_RESULTS, 0, ebufp);
+	PIN_FLIST_FLD_SET(flistp, PIN_FLD_TYPE,(void *)NULL, ebufp);
         
-	PIN_ERR_LOG_FLIST(PIN_ERR_LEVEL_DEBUG, "op_get_meta_field search flistp", items_flistp);  
+	PIN_ERR_LOG_FLIST(PIN_ERR_LEVEL_DEBUG, "op_get_meta_field search flistp", search_flistp);  
 
-	 PCM_OP(ctxp, PCM_OP_SEARCH, 0, items_flistp, out_flistpp, ebufp);
+	 PCM_OP(ctxp, PCM_OP_SEARCH, 0, search_flistp, out_flistpp, ebufp);
 
-	PIN_ERR_LOG_FLIST(PIN_ERR_LEVEL_DEBUG, "op_get_meta_field search flistp", *out_flistpp);     
- 
-  }
+	PIN_ERR_LOG_FLIST(PIN_ERR_LEVEL_DEBUG, "op_get_meta_field search flistp", *out_flistpp);
 	}
+	     
+ 	}
+
