759dc87fea0da633775b9fd7697010d7bc550bf8
Prabha1991 <prabha.muthusamy@aaravsolution.comSecret@123>
2016-03-22 12:03:57 +0530

    new MODIFY opcode

--- brm_metafields/src/fm_metafield/fm_metafield_pol_modify_metafield.c
+++ brm_metafields/src/fm_metafield/fm_metafield_pol_modify_metafield.c
@@ -1,27 +1,4 @@
 /* continuus file information --- %full_filespec: fm_metafield_pol_modify_metafield.c~5:csrc:1 % */
-/*******************************************************************
- *
- *  @(#) %full_filespec: fm_bill_make_invoice.c~5:csrc:1 %
- *
- *      Copyright (c) 1999 - 2006  Oracle. All rights reserved.
- *
- *      This material is the confidential property of Oracle Corporation
- *      or its subsidiaries or licensors and may be used, reproduced, stored
- *      or transmitted only in accordance with a valid Oracle license or
- *      sublicense agreement.
- *
- *******************************************************************/
-
-/******************************************************************
- ******************************************************************
- **
- ** This policy opcode can be used to perform validations over and
- ** above those which will be performed by fm_price
- **
- ** By default it just passes 
- **
- ******************************************************************
- ******************************************************************/
 
 #ifndef lint
 static  char    Sccs_id[] = "@(#)%Portal Version: fm_metafield_pol_modify_metafield.c:ServerIDCVelocityInt:1:2006-Sep-06 16:46:27 %";
@@ -147,34 +124,23 @@ fm_modify_meta_field(
         pin_flist_t             **out_flistpp,
         pin_errbuf_t            *ebufp)
 {
-        pin_flist_t     *items_flistp = NULL;
         pin_flist_t     *flistp = NULL; 
-        poid_t          *bi_pdp = NULL; 
         int32           status = 0;
         int64           database = 0;
         int32           sflags = 256;
         int32           result = PIN_BOOLEAN_FALSE;
-        void            *vp = NULL;
         pin_decimal_t   *zero_decimal =  pin_decimal("0.0", ebufp);
 	pin_flist_t	*r_flistp = NULL;
-	int32          *bi_name = NULL;
 
         if (PIN_ERR_IS_ERR(ebufp)) {
                 return;
         }
         PIN_ERR_CLEAR_ERR(ebufp);
 
-       bi_pdp = PIN_FLIST_FLD_GET(in_flistp, PIN_FLD_POID, 0, ebufp );
-       bi_name = PIN_FLIST_FLD_GET(in_flistp, PIN_FLD_CLASS_NAME, 0, ebufp);
-			   
-        items_flistp = PIN_FLIST_CREATE(ebufp);
-        database = PIN_POID_GET_DB(bi_pdp);
-        PIN_FLIST_FLD_SET(items_flistp, PIN_FLD_POID, (void *) bi_pdp, ebufp);
-	PIN_FLIST_FLD_SET(items_flistp, PIN_FLD_CLASS_NAME, (void *) bi_name, ebufp);
 
-         PCM_OP(ctxp, PCM_OP_WRITE_FLDS, 0, items_flistp, out_flistpp, ebufp);
+         PCM_OP(ctxp, PCM_OP_WRITE_FLDS, 0, in_flistp, out_flistpp, ebufp);
 
-	PIN_ERR_LOG_FLIST(PIN_ERR_LEVEL_DEBUG, "op_modify_meta_field search flistp", items_flistp);  
+	PIN_ERR_LOG_FLIST(PIN_ERR_LEVEL_DEBUG, "op_modify_meta_field search flistp", in_flistp);  
 
 
  
